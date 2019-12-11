6700fe03de82d8a3ccd2cdbd1421033468ba6dd9
Safi Ahlberg <safiahlberg@users.noreply.github.com>
2019-12-11 10:41:01 +0100

    Added an example with fallback

--- src/main/java/com/wixia/common/config/App.java
+++ src/main/java/com/wixia/common/config/App.java
@@ -19,9 +19,26 @@ public class App
         System.out.println(String.format("compound1.val1=%s", conf.getString("compound1.val1")));
         System.out.println(String.format("compound1.val2=%s", conf.getString("compound1.val2")));
 
+        // This resets the config to use everything prefixed with pref1, but in this case, the global values are lost
+        System.out.println("\nReset the configuration to point at pref1");
         conf = conf.getConfig("pref1");
 
         System.out.println(String.format("compound1.val1=%s", conf.getString("compound1.val1")));
         System.out.println(String.format("compound1.val2=%s", conf.getString("compound1.val2")));
+
+        // This would throw an exception
+        // System.out.println(String.format("globalVal=%s", conf.getString("globalVal")));
+
+        // This resets the config to use everything prefixed with pref1 and use other config as fallback
+        conf = ConfigFactory.load();
+        System.out.println("\nReload configuration");
+        conf = conf.getConfig("pref1").withFallback(conf);
+
+        System.out.println(String.format("compound1.val1=%s", conf.getString("compound1.val1")));
+        System.out.println(String.format("compound1.val2=%s", conf.getString("compound1.val2")));
+
+        // This can now be done without an exception
+        System.out.println(String.format("globalVal=%s", conf.getString("globalVal")));
+
     }
 }
