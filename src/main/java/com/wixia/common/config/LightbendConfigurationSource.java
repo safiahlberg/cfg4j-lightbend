06f23dbce7ab0c959782245c95995ec56c69035f
Safi Ahlberg <safiahlberg@users.noreply.github.com>
2019-12-17 14:21:20 +0100

    Added documentation

--- src/main/java/com/wixia/common/config/LightbendConfigurationSource.java
+++ src/main/java/com/wixia/common/config/LightbendConfigurationSource.java
@@ -8,6 +8,11 @@ import java.util.Properties;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Note: use {@link LightbendConfigurationSourceBuilder} for building instances of this class.
+ * <p>
+ * Read configuration using Lightbend Config, see https://github.com/lightbend/config
+ */
 public class LightbendConfigurationSource implements ConfigurationSource {
 
     private final ConfigFactoryHandler configFactoryHandler;
@@ -15,11 +20,27 @@ public class LightbendConfigurationSource implements ConfigurationSource {
 
     private boolean initialized;
 
+    /**
+     * Note: use {@link LightbendConfigurationSourceBuilder} for building instances of this class.
+     * <p>
+     * Read configuration using Lightbend Config, which has various ways to read configurations, this can
+     * be configured with the builder class.
+     *
+     * @param configFactoryHandler {@link LightbendConfigFactoryHandler} the handler class that helps
+     *                             with creating the load strategy
+     */
     LightbendConfigurationSource(ConfigFactoryHandler configFactoryHandler) {
         this.configFactoryHandler = requireNonNull(configFactoryHandler, "Argument 'configFactoryHandler' must not be null");
         init();
     }
 
+    /**
+     * (Re)loads the configuration ({@link Config}) using Lightbend, and converts it to a
+     * local {@link Properties} object.
+     *
+     * @param environment {@Link org.cfg4j.source.context.environment.Environment} not used
+     * @return the populated {@link Properties} object
+     */
     @Override
     public Properties getConfiguration(Environment environment) {
         if (!initialized) {
@@ -35,12 +56,20 @@ public class LightbendConfigurationSource implements ConfigurationSource {
         return config;
     }
 
+    /**
+     * Use the {@link LightbendConfigFactoryHandler#init()} to create a
+     * Lightbend {@link Config} object, which is cached.
+     */
     @Override
     public void init() {
         lightbendConfig = configFactoryHandler.init();
         initialized = true;
     }
 
+    /**
+     * Use the {@link LightbendConfigFactoryHandler#reload()} to create a
+     * Lightbend {@link Config} object, which is cached.
+     */
     @Override
     public void reload() {
         lightbendConfig = configFactoryHandler.reload();
