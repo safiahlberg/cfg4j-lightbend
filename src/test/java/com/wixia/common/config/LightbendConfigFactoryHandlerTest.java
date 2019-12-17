b2e109a721fd6af1c68ca8babcd6075549ca94fa
Safi Ahlberg <safiahlberg@users.noreply.github.com>
2019-12-17 14:05:39 +0100

    Added test

---
+++ src/test/java/com/wixia/common/config/LightbendConfigFactoryHandlerTest.java
@@ -0,0 +1,16 @@
+package com.wixia.common.config;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class LightbendConfigFactoryHandlerTest {
+
+    @Test
+    public void testInit() {
+    }
+
+    @Test
+    public void testReload() {
+    }
+}
