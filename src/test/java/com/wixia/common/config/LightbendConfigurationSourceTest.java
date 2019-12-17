648ed376f0b8734b8a21abd2f047fbae27dd1932
Safi Ahlberg <safiahlberg@users.noreply.github.com>
2019-12-17 13:53:12 +0100

    Added some tests

--- src/test/java/com/wixia/common/config/LightbendConfigurationSourceTest.java
+++ src/test/java/com/wixia/common/config/LightbendConfigurationSourceTest.java
@@ -1,9 +1,25 @@
 package com.wixia.common.config;
 
+import com.typesafe.config.Config;
+import mockit.*;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Properties;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 @Test
 public class LightbendConfigurationSourceTest {
+
+    @Tested
+    LightbendConfigurationSource tested;
+    @Injectable
+    ConfigFactoryHandler mockConfigFactoryHandler;
+    @Mocked
+    Config mockConfig;
 
     @Test(expectedExceptions = NullPointerException.class)
     public void testConstructorWithNull() {
@@ -11,14 +27,61 @@ public class LightbendConfigurationSourceTest {
     }
 
     @Test
+    public void testConstructorAndInit() {
+        LightbendConfigurationSource configurationSource = new LightbendConfigurationSource(mockConfigFactoryHandler);
+
+        new Verifications() {{
+            mockConfigFactoryHandler.init();
+            times = 2;
+        }};
+    }
+
+    @Test
     public void testGetConfiguration() {
+        final HashMap configMap = new HashMap();
+        configMap.put("key1", "value1");
+        configMap.put("key2", "value2");
+
+        new Expectations() {{
+            mockConfig.entrySet();
+            result = configMap.entrySet();
+        }};
+
+        Properties configProperties = tested.getConfiguration(null);
+
+        assertTrue(configProperties.containsKey("key1"));
+        assertTrue(configProperties.containsKey("key2"));
+        assertEquals(configProperties.size(), 2);
+
+        new Verifications() {{
+            mockConfigFactoryHandler.reload();
+            times = 1;
+
+            mockConfig.entrySet();
+            times = 1;
+        }};
     }
 
     @Test
     public void testInit() {
+        tested.init();
+
+        new Verifications() {{
+            mockConfigFactoryHandler.init();
+            times = 2;
+        }};
     }
 
     @Test
     public void testReload() {
+        tested.reload();
+
+        new Verifications() {{
+            mockConfigFactoryHandler.init();
+            times = 1;
+
+            mockConfigFactoryHandler.reload();
+            times = 1;
+        }};
     }
 }
